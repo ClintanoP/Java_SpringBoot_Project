c45e37fd87959ef33ff950d0bb62dddca2497b78
ClintanoP <clintanoperrins@gmail.com>
2022-10-12 20:58:03 +0100

    contact form event firers

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -6,6 +6,8 @@ import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Contact;
 import com.example.application.data.entity.Status;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -16,6 +18,8 @@ import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
 
 public class ContactForm extends FormLayout{
     Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
@@ -69,9 +73,60 @@ public class ContactForm extends FormLayout{
         delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
         cancel.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, contact)));
+        cancel.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
         save.addClickShortcut(Key.ENTER);
         cancel.addClickShortcut(Key.ESCAPE);
         
         return new HorizontalLayout(save, delete, cancel);
     }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(contact);
+            fireEvent(new SaveEvent(this, contact));
+        }catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+// Events
+public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+    private Contact contact;
+  
+    protected ContactFormEvent(ContactForm source, Contact contact) { 
+      super(source, false);
+      this.contact = contact;
+    }
+  
+    public Contact getContact() {
+      return contact;
+    }
+  }
+  
+  public static class SaveEvent extends ContactFormEvent {
+    SaveEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+  }
+  
+  public static class DeleteEvent extends ContactFormEvent {
+    DeleteEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+  
+  }
+  
+  public static class CloseEvent extends ContactFormEvent {
+    CloseEvent(ContactForm source) {
+      super(source, null);
+    }
+  }
+  
+  public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType,
+      ComponentEventListener<T> listener) { 
+    return getEventBus().addListener(eventType, listener);
+  }
 }
